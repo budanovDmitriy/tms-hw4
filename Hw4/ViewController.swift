c802f94a1898ee2bf3c90a671a5a88006537d615
DmitryBudanov <dmitriybudanov@MacBook-Pro-qqq.local>
2022-02-14 10:14:07 +0300

    Initial commit

---
+++ Hw4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hw4
+//
+//  Created by Dmitriy Budanov on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
