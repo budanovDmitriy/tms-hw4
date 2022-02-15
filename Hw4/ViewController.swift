7e911f50104bfa5e77632dc3d82f9bf577b9c26c
DmitryBudanov <dmitriybudanov@MacBook-Pro-qqq.local>
2022-02-16 01:03:46 +0300

    Fix the names of variables

--- Hw4/ViewController.swift
+++ Hw4/ViewController.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 class ViewController: UIViewController {
-    var x = [(1, "x"), (4, "y"), (6, "a")]
-    var y = [(5, "x"), (4, "f"), (10, "b"), (20,"f")]
-    var g = [("x", 5), ("g", 10),("a",44)]
+    var firstArrayOfColage = [(1, "x"), (4, "y"), (6, "a")]
+    var secondArrayOfColage = [(5, "x"), (4, "f"), (10, "b"), (20,"f")]
+    var thirdArrayOfColage = [("x", 5), ("g", 10),("a",44)]
     func exponentiation(number num:Int, Power power:Int = 2) -> Int {
         var numberToThePower:Int = 1
         for _ in 1...power {
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
         }
         return numberToThePower
     }
-    func sortcolage(colage : [(Int,String)]){
-    let newArray = colage.map {element in
+    func sortArrayOfColage(array : [(Int,String)]){
+    let newArray = array.map {element in
         (exponentiation(number: element.0),element.1)
         }.filter { element in
             element.0 % 2 == 0
@@ -27,8 +27,8 @@ class ViewController: UIViewController {
         }
         print(newArray)
     }
-    func sortcolage(colage : [(String,Int)]){
-    let newArray = colage.map {element in
+    func sortArrayOfColage(array : [(String,Int)]){
+    let newArray = array.map {element in
         (element.0,exponentiation(number: element.1))
         }.filter { element in
             element.1 % 2 == 0
@@ -40,9 +40,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        sortcolage(colage:g)
-        sortcolage(colage:y)
-        sortcolage(colage:x)
+        sortArrayOfColage(array:firstArrayOfColage)
+        sortArrayOfColage(array:secondArrayOfColage)
+        sortArrayOfColage(array:thirdArrayOfColage)
 }
    
 }
